e45841863ba9ee295930706f32d4802426e8dccc
Sathish Kumar Krishnan <sathiskr@icloud.com>
2019-06-02 08:19:54 +0530

    Initial Commit

---
+++ ARMagic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARMagic
+//
+//  Created by Sathishkumar Krishnan on 6/2/19.
+//  Copyright © 2019 Sathishkumar Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
